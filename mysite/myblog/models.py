6e92d0a4fc5b9e653fc8e3781f2f05869b3d552c
DonatasNoreika <donoras@gmail.com>
2025-08-28 19:32:10 +0300

    Paspaudus įrašą, patenkama į jo DetailView

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=True)
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -7,3 +7,8 @@ class PostListView(generic.ListView):
     template_name = "posts.html"
     context_object_name = 'posts'
 
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = "post.html"
+    context_object_name = "post"
+
