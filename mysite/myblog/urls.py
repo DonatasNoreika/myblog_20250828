4804afc85cd9cc20e929860a6c63966030f43cf6
DonatasNoreika <donoras@gmail.com>
2025-08-28 19:52:12 +0300

    Pridėta straipsnių paieška

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('search', views.search, name='search'),
     path("", views.PostListView.as_view(), name="posts"),
     path("posts/<int:pk>", views.PostDetailView.as_view(), name="post"),
 ]
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from django.db.models import Q
+
+def search(request):
+    query = request.GET.get('query')
+    post_search_results = Post.objects.filter(
+        Q(title__icontains=query) | Q(author__username__icontains=query))
+    context = {
+        'query': query,
+        'posts': post_search_results,
+    }
+    return render(request, template_name="search.html", context=context)
 
 class PostListView(generic.ListView):
     model = Post
     template_name = "posts.html"
     context_object_name = 'posts'
+    paginate_by = 10
 
 class PostDetailView(generic.DetailView):
     model = Post
