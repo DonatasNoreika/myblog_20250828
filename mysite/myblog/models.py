c717b9925e614c06d02ad9dab2947f08d76ac198
DonatasNoreika <donoras@gmail.com>
2025-09-04 19:17:53 +0300

    Vartotojo nuotraukos rodomos prie straipsnių ir komentarų

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -2,9 +2,22 @@ from django.contrib.auth.models import User
 from django.db import models
 from tinymce.models import HTMLField
 from django.contrib.auth.models import AbstractUser
+from PIL import Image
 
 class CustomUser(AbstractUser):
     photo = models.ImageField(upload_to="profile_pics", null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.photo.path)
+        min_side = min(img.width, img.height)
+        left = (img.width - min_side) // 2
+        top = (img.height - min_side) // 2
+        right = left + min_side
+        bottom = top + min_side
+        img = img.crop((left, top, right, bottom))
+        img = img.resize((300, 300), Image.LANCZOS)
+        img.save(self.photo.path)
 
 class Post(models.Model):
     title = models.CharField()
