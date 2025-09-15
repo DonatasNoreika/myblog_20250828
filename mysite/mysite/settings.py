73755c5f08d4534023a8978ff97895d7c8f98dc2
DonatasNoreika <donoras@gmail.com>
2025-09-15 19:19:34 +0300

    pridėtas STATIC_ROOT

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -130,15 +130,18 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'myblog/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'myblog/media')
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'myblog/media')
-
-MEDIA_URL = '/media/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
